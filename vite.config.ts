480a7cbe3e3a9851c002eb534c31d869e88aaf26
Egor Skleynov <e.skleynov@adictgroup.ru>
2026-03-31 23:09:14 +0800

    feat: добавляет автодеплой проекта на GitHub Pages

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, loadEnv } from 'vite'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const base = env.VITE_BASE_PATH?.trim() || '/'
+
+  return {
+    base,
+  }
+})
